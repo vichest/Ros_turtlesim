ebed4e888e6ba51720cdb006a105bf3ea7904bc6
sanjit pandit <sanjitpandit56@gmail.com>
2024-11-09 18:58:52 +0530

    Delete build directory

--- build/ros_session/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sanjit/ros_ws/src/ros_session
-# Build directory: /home/sanjit/ros_ws/build/ros_session
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
